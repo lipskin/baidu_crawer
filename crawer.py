807721e92a9479f630755115c5f6431a2c22a784
adu windows <duuyidong@gmail.com>
2018-06-12 20:46:07 +0800

    [Yidong]get content page links

--- crawer.py
+++ crawer.py
@@ -4,7 +4,7 @@
 import requests
 from bs4 import BeautifulSoup
 import re
-
+import pdb
 
 def get_baidu_links(page_url):
     # page_url = "http://www.91baiduyun.com/thread-38640-166-1.html"
@@ -19,7 +19,12 @@ def main():
     url = "http://www.91baiduyun.com/thread-38640-1-1.html"
     soup = BeautifulSoup(requests.get(url).content, "lxml")
     last_page_url = [ a["href"] for a in soup.find("div", class_="pg").find_all("a")][-2]
-    get_baidu_links(last_page_url)
+    last_url_id = re.findall(r'\d+', last_page_url)[-2]
+    page_urls = [last_page_url]
+    page_urls.append(last_page_url.replace(last_url_id, str(int(last_url_id)-1)))
+    page_urls.append(last_page_url.replace(last_url_id, str(int(last_url_id)-2)))
+    for page_url in page_urls:
+        get_baidu_links(page_url)
 
 
 if __name__ == '__main__':
