83954aeec4ca3d786536eed8175ea010e8068d7f
adu windows <duuyidong@gmail.com>
2018-06-12 20:28:37 +0800

    [Yidong]find last page

--- crawer.py
+++ crawer.py
@@ -7,7 +7,7 @@ import re
 
 
 def get_baidu_links(page_url):
-    page_url = "http://www.91baiduyun.com/thread-38640-166-1.html"
+    # page_url = "http://www.91baiduyun.com/thread-38640-166-1.html"
     soup = BeautifulSoup(requests.get(page_url).content, "lxml")
     links = [re.findall(r'\https://pan.baidu.com.*', content.text)[0] for content in soup.find_all("td", class_ = "t_f")]
     for link in links:
@@ -17,7 +17,9 @@ def get_baidu_links(page_url):
 
 def main():
     url = "http://www.91baiduyun.com/thread-38640-1-1.html"
-    get_baidu_links()
+    soup = BeautifulSoup(requests.get(url).content, "lxml")
+    last_page_url = [ a["href"] for a in soup.find("div", class_="pg").find_all("a")][-2]
+    get_baidu_links(last_page_url)
 
 
 if __name__ == '__main__':
